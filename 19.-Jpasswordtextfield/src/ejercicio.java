341f5774590f9b99678d9667c8ec643872bcba7d
Tronh99 <guns_angel_roses@hotmail.com>
2024-10-23 20:20:08 -0600

    Jpassword

--- 19.-Jpasswordtextfield/src/ejercicio.java
+++ 19.-Jpasswordtextfield/src/ejercicio.java
@@ -1,11 +1,12 @@
 import java.awt.FlowLayout;
-
-import javax.swing.BoxLayout;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class ejercicio {
 	
@@ -13,20 +14,28 @@ public class ejercicio {
 		
 	}
 	
-	public void dialogo2Botones() {
-		new JOptionPane();
-		int boton02 = JOptionPane.showConfirmDialog(new JFrame(), "Segundo evento con JOptionFrame", "Terecer evento mas opciones", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
-	}
-	
 	public void seguridad() {
 		
 		JFrame ventana = new JFrame();
 		ventana.setTitle("Inicio de sesion.");
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventana.setLayout(new BoxLayout(ventana.getContentPane(), BoxLayout.Y_AXIS));
 		
+		JPanel panelNorte	= new JPanel();
+		JPanel panelCentro	= new JPanel();
+		JLabel etiqueta1 = new JLabel();
+		JLabel etiqueta2 = new JLabel();
+		JLabel etiqueta3 = new JLabel();
 		JPanel pcaja1 = new JPanel();
 		JPanel pcaja2 = new JPanel();
+		JButton btn1 = new JButton();
+		JButton btn2 = new JButton();
+		
+		
+		panelCentro.setLayout(new GridLayout(3,3));
+		
+		etiqueta1.setText("Inicio de sesion.");
+		etiqueta2.setText("Usuario.");
+		etiqueta3.setText("Contraseña.");
 		
 		pcaja1.setLayout(new FlowLayout());
 		
@@ -34,17 +43,31 @@ public class ejercicio {
 		caja1.setText("Nombre");
 		
 		JPasswordField caja2 = new JPasswordField();
-		caja2.setText("Contraseña");
+		caja2.setText("      ");
+		
+		btn1.setText("Aceptar");
+		btn2.setText("Cancelar");
 		
 		
 		pcaja1.add(caja1);
 		pcaja2.add(caja2);
 		
-		ventana.add(pcaja1);
-		ventana.add(pcaja2);
+		panelNorte.add(etiqueta1);
+		panelCentro.add(etiqueta2);
+		panelCentro.add(pcaja1);
+		panelCentro.add(etiqueta3);
+		panelCentro.add(pcaja2);
+		panelCentro.add(btn1);
+		panelCentro.add(btn2);
 		
 		
+//		ventana.add(pcaja1);
+		ventana.add(panelNorte, BorderLayout.NORTH);
+		ventana.add(panelCentro, BorderLayout.CENTER);
+				
 		ventana.pack();
+//		ventana.setSize(600,300);
+		ventana.setLocation(300, 350);
 		ventana.setVisible(true);
 		
 	}
