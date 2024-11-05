cc2d3b4e5288c0146625f30aa2c39dfe6479a17f
guns_ <guns_@CHIKLESPOWER>
2024-11-05 12:22:47 -0600

    EJERCICIO 25

---
+++ 26.-JscrollPane/src/index.java
@@ -0,0 +1,10 @@
+
+public class index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JTableScrollPane jts = new JTableScrollPane();
+		jts.tablaMejorada();
+	}
+
+}
